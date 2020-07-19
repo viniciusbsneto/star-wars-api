847dcc2058205c4d272923ad8af06957b9a9fd2d
Vinícius Neto <viniciusbsneto@gmail.com>
2020-07-19 06:33:44 -0300

    chore: Basic REST API structure with mock data

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/viniciusbsneto/star-wars-rest-api
+
+go 1.14
+
+require (
+	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/gorilla/mux"
+)
+
+type Planet struct {
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Climate []string `json:"climate"`
+	Terrain []string `json:"terrain"`
+	Films   int      `json:"films"`
+}
+
+var planets []Planet
+
+func getPlanets(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(responseWriter).Encode(planets)
+}
+
+func getPlanetById(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for _, item := range planets {
+		if item.ID == params["id"] {
+			json.NewEncoder(responseWriter).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(responseWriter).Encode(&Planet{})
+}
+
+func createPlanet(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	var planet Planet
+	_ = json.NewDecoder(request.Body).Decode(&planet)
+	planet.ID = uuid.NewV4().String()
+	planets = append(planets, planet)
+	json.NewEncoder(responseWriter).Encode(planet)
+}
+
+func updatePlanet(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for index, item := range planets {
+		if item.ID == params["id"] {
+			planets = append(planets[:index], planets[index+1:]...)
+			var planet Planet
+			_ = json.NewDecoder(request.Body).Decode(&planet)
+			planet.ID = params["id"]
+			planets = append(planets, planet)
+			json.NewEncoder(responseWriter).Encode(planet)
+			return
+		}
+	}
+	json.NewEncoder(responseWriter).Encode(planets)
+}
+
+func deletePlanet(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for index, item := range planets {
+		if item.ID == params["id"] {
+			planets = append(planets[:index], planets[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(responseWriter).Encode(planets)
+}
+
+func main() {
+
+	router := mux.NewRouter()
+
+	planets = append(planets, Planet{ID: "1", Name: "Tatooine", Climate: []string{"Arid"}, Terrain: []string{"Dessert"}, Films: 5})
+	planets = append(planets, Planet{ID: "2", Name: "Alderaan", Climate: []string{"Temperate"}, Terrain: []string{"Grasslands", "Mountain"}, Films: 2})
+
+	router.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+		json.NewEncoder(responseWriter).Encode(map[string]string{"message": "Hello World"})
+	}).Methods("GET")
+
+	router.HandleFunc("/planets", getPlanets).Methods("GET")
+	router.HandleFunc("/planets/{id}", getPlanetById).Methods("GET")
+	router.HandleFunc("/planets", createPlanet).Methods("POST")
+	router.HandleFunc("/planets/{id}", updatePlanet).Methods("PUT")
+	router.HandleFunc("/planets/{id}", deletePlanet).Methods("DELETE")
+
+	server := &http.Server{
+		Handler:      router,
+		Addr:         "localhost:3333",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
